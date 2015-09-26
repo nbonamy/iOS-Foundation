f326332d0b2f0e048b263e3323c466e2c3ec5def
Nicolas Bonamy <nicolas@bonamy.fr>
2015-09-26 14:05:23 +0200

    Added ios8/ios9 macros

--- Pod/FoundationMacros.h
+++ Pod/FoundationMacros.h
@@ -12,11 +12,12 @@
 #define IPHONE_6PLUS_WIDTH	414
 
 // devide info
-#define IS_IPAD()					(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
-#define IS_SHORT_IPHONE()	([UIScreen mainScreen].bounds.size.height == SHORT_IPHONE_HEIGHT)
-#define IS_TALL_IPHONE()	([UIScreen mainScreen].bounds.size.height == TALL_IPHONE_HEIGHT)
-#define IS_IPHONE6()			([UIScreen mainScreen].bounds.size.height == IPHONE6_HEIGHT)
-#define IS_IPHONE6_PLUS()	([UIScreen mainScreen].bounds.size.height == IPHONE6PLUS_HEIGHT)
+#define IS_IPAD()							(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#define IS_SHORT_IPHONE()			([UIScreen mainScreen].bounds.size.height == SHORT_IPHONE_HEIGHT)
+#define IS_TALL_IPHONE()			([UIScreen mainScreen].bounds.size.height == TALL_IPHONE_HEIGHT)
+#define IS_IPHONE6()					([UIScreen mainScreen].bounds.size.height == IPHONE6_HEIGHT)
+#define IS_IPHONE6_PLUS()			([UIScreen mainScreen].bounds.size.height == IPHONE6PLUS_HEIGHT)
+#define IS_VERY_TALL_IPHONE()	([UIScreen mainScreen].bounds.size.height >= IPHONE6_HEIGHT)
 
 // os info
 #define IS_RETINA()       ([[UIScreen mainScreen] respondsToSelector:@selector(scale)] == YES && [[UIScreen mainScreen] scale] == 2.00)
@@ -24,6 +25,9 @@
 #define IS_IOS_7()				([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0 && [[[UIDevice currentDevice] systemVersion] floatValue] < 8.0)
 #define IS_IOS_8()				([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0 && [[[UIDevice currentDevice] systemVersion] floatValue] < 9.0)
 #define IS_IOS_80()				([[[UIDevice currentDevice] systemVersion] floatValue] == 8.0)
+
+#define IS_IOS_8_OR_LATER()	([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+#define IS_IOS_9_OR_LATER()	([[[UIDevice currentDevice] systemVersion] floatValue] >= 9.0)
 
 // dynamic properties
 #define ADD_DYNAMIC_PROPERTY(PROPERTY_TYPE,PROPERTY_NAME,SETTER_NAME) \
