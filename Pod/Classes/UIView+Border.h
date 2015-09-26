77b25139aa3c13f34657ca5372c8f9605f746cf3
Nicolas Bonamy <nicolas@bonamy.fr>
2015-09-26 14:05:07 +0200

    Added border category on views

---
+++ Pod/Classes/UIView+Border.h
@@ -0,0 +1,18 @@
+//
+//  UIView+Border.h
+//  Foundation
+//
+//  Created by Nicolas Bonamy on 08/08/2015.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIView (Border)
+
+- (void) addTopBorderWithColor:(UIColor*) color andWidth:(CGFloat) borderWidth;
+- (void) addBottomBorderWithColor:(UIColor*) color andWidth:(CGFloat) borderWidth;
+- (void) addLeftBorderWithColor:(UIColor*) color andWidth:(CGFloat) borderWidth;
+- (void) addRightBorderWithColor:(UIColor*) color andWidth:(CGFloat) borderWidth;
+
+@end
